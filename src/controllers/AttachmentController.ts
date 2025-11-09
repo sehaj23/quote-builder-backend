9e29dd0402c3704a1d9c434be2896ff7c18d95fb
sehaj23 <sehaj23chawla@gmail.com>
2025-11-09 14:41:10 +1300

    Region bucket

--- src/controllers/AttachmentController.ts
+++ src/controllers/AttachmentController.ts
@@ -32,7 +32,7 @@ export class AttachmentController {
   ) {
     
     this.s3Client = new S3Client({
-      region: process.env.AWS_REGION || 'us-east-1',
+      region: process.env.AWS_S3_REGION || 'ap-southeast-2',
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
